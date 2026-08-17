81a57ac388961781359ac3e20dc0c0fa7dcb9301
tellsx <tel@nezh.dpdns.org>
2026-08-17 20:25:57 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -82,6 +82,7 @@ UPDATE_PACKAGE "vnt" "lmq8267/luci-app-vnt" "main"
 
 
 UPDATE_PACKAGE "luci-app-daed" "QiuSimons/luci-app-daed" "kix"
+UPDATE_PACKAGE "luci-app-daede" "kenzok8/openwrt-daede" "main"
 UPDATE_PACKAGE "luci-app-dae" "davidtall/luci-app-dae" "kix"
 UPDATE_PACKAGE "luci-app-pushbot" "zzsj0928/luci-app-pushbot" "master"
 UPDATE_PACKAGE "luci-app-nginx-manager" "hello-yunshu/luci-app-nginx-manager" "main"
